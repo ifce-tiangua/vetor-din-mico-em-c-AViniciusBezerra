b47543b62d8e6db505d686fa2be3cc9f47267f28
AViniciusBezerra <vinicius.bezerra2564@gmail.com>
2024-11-19 15:21:55 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main(){
     int tamanho;
     int *vetor;
 
@@ -22,6 +22,5 @@ int main() {
     
     free(vetor);
     vetor = NULL;
-
     return 0;
 }
